78706338367d8e3c94fa7a1bc034f0cedbbdb602
OleksandrVasylchuk <73894374+OleksandrVasylchuk@users.noreply.github.com>
2021-07-26 02:49:54 +0300

    movies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { lazy, Suspense } from "react";
+import { Switch, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Loader from "react-loader-spinner";
+import Container from "./Components/Container/Container";
+import AppBar from "./Components/AppBar/AppBar";
+
+// const AppBar = lazy(() =>
+//   import("./components/AppBar/" /* webpackChunkName: "app-bar" */)
+// );
+
+const HomeView = lazy(() =>
+  import("./views/HomePage" /* webpackChunkName: "home-view" */)
+);
+
+const MoviesView = lazy(() =>
+  import("./views/MoviesPage" /* webpackChunkName: "mvies-view" */)
+);
+
+const HomeSubView = lazy(() =>
+  import("./views/MovieDetailsPage" /* webpackChunkName: "home-sub-view" */)
+);
+
+const NotFoundView = lazy(() =>
+  import("./views/NotFound" /* webpackChunkName: "not-found-view" */)
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Suspense
+        fallback={
+          <div style={{ textAlign: "center", marginTop: "80px" }}>
+            <Loader
+              type="Circles"
+              color="#00BFFF"
+              height={100}
+              width={100}
+              timeout={3000} //3 secs
+            />
+          </div>
+        }
+      >
+        <AppBar />
+
+        <Switch>
+          <Route path="/" exact>
+            <HomeView />
+          </Route>
+
+          <Route path="/movies" exact>
+            <MoviesView />
+          </Route>
+
+          <Route path="/movies/:moviesId">
+            <HomeSubView />
+          </Route>
+
+          <Route>
+            <NotFoundView />
+          </Route>
+        </Switch>
+      </Suspense>
+
+      <ToastContainer autoClose={3000} />
+    </Container>
   );
 }
 
